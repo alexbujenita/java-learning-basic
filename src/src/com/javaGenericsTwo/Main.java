4705cc7fd0c239e8e091c1a74e80e7e3524affba
Alex <dekc89@gmail.com>
2020-01-26 14:41:15 +0000

    generics part 2

--- src/src/com/javaGenericsTwo/Main.java
+++ src/src/com/javaGenericsTwo/Main.java
@@ -7,12 +7,27 @@ public class Main {
         BaseballPlayer kyle = new BaseballPlayer("Kyle");
         SoccerPlayer paul = new SoccerPlayer("Paul");
 
-        Team bassano = new Team("BdG");
+        Team<FootballPlayer> bassano = new Team<>("BdG"); // adding the type argument, now this team will only accept FootballPlayer
+        Team<FootballPlayer> mestre = new Team<>("Mestre");
+        mestre.addPlayer(new FootballPlayer("Ignazio"));
         bassano.addPlayer(joe);
-        bassano.addPlayer(joe);
-        bassano.addPlayer(kyle);
-        bassano.addPlayer(paul);
-
+//        bassano.addPlayer(kyle);
+//        bassano.addPlayer(paul);
         System.out.println(bassano.numPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Bats only");
+        baseballTeam.addPlayer(kyle);
+        System.out.println(baseballTeam.numPlayers());
+
+        Team<SoccerPlayer> plot = new Team<>("PLOT");
+        plot.addPlayer(paul);
+        System.out.println(plot.numPlayers());
+
+        bassano.matchResult(mestre, 30, 20);
+//        bassano.matchResult(baseballTeam, 30, 30);
+//        baseballTeam.matchResult(plot, 10, 20);
+//        bassano.matchResult(plot, 100, 20);
+//        plot.matchResult(baseballTeam, 30, 900);
+        System.out.println(bassano.ranking());
     }
 }
--- src/src/com/javaGenericsTwo/Team.java
+++ src/src/com/javaGenericsTwo/Team.java
@@ -2,14 +2,32 @@ package com.javaGenericsTwo;
 
 import java.util.ArrayList;
 
-public class Team {
+public class Team<T extends Player> { // to indicate that's going to have a type, and by extending we say the type of class to use for team, Player or any Player subclass
+    // can also implement and extend, EG extends Player & Coach & Ref, coach and ref are interfaces in this case
     private String name;
     int played = 0;
     int won = 0;
     int lost = 0;
     int tied = 0;
 
-    private ArrayList<Player> members = new ArrayList<>(); // no need to specify the generic when using Java >8, we can use the diamond notation "<>"
+//    private ArrayList<Player> members = new ArrayList<>(); // no need to specify the generic when using Java >8, we can use the diamond notation "<>"
+
+    private ArrayList<T> members = new ArrayList<>();
+
+    /*
+    And how this basically works is when we
+    actually instantiate a class the T will be
+    replaced automatically by Java
+    with the actual class we're using,
+    with the real type in other words when
+    we're actually instantiating the class.
+    And that's why we've actually
+    changed the addPlayer and
+    the ArrayList as well, so
+    all three changed so that we can actually,
+    we're able to actually do
+    that sort of type checking.
+    */
 
     public Team(String name) {
         this.name = name;
@@ -19,7 +37,24 @@ public class Team {
         return name;
     }
 
-    public boolean addPlayer(Player player) {
+    public boolean addPlayer(T player) {
+        /*
+        And because this type's not known until
+        we actually instantiate a class in other
+        words at run time, there's actually no way
+        for Java to know that an object of type
+        T in this case really does
+        have a get name method.
+
+        So what we're going to do is we're gonna
+        cast player the player parameterize type
+        to make that a player type before
+        attempting to evoke its getName method.
+        System.out.println(((Player) player).getName() + " is already on this team.");
+        TEMPORARY
+
+        AFTER T extends Player we can remove the casting
+         */
         if(members.contains(player)) {
             System.out.println(player.getName() + " is already on this team.");
             return false;
@@ -34,16 +69,23 @@ public class Team {
         return this.members.size();
     }
 
-    public void matchResult(Team opponent, int ourScore, int theirScore) {
+    public void matchResult(Team<T> opponent, int ourScore, int theirScore) {
+
+        String message;
+
         if(ourScore > theirScore) {
             won++;
+            message = " beat ";
         } else if(ourScore < theirScore) {
             lost++;
+            message = " lost to ";
         } else {
             tied++;
+            message = " drew with ";
         }
         played++;
         if(opponent != null) {
+            System.out.println(this.getName() + message + opponent.getName());
             opponent.matchResult(null, theirScore, ourScore);
         }
     }
