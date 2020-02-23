f14a51dadfe4f2c05b9b1b6058c69f840d8ccafd
Alex <dekc89@gmail.com>
2020-02-23 15:51:43 +0000

    Map adv game part one

--- src/src/javaCollections/MapProgram.java
+++ src/src/javaCollections/MapProgram.java
@@ -10,8 +10,11 @@ public class MapProgram {
         languages.put("Java", "a compiled high level...");
         languages.put("Python", "an interpreted high level...");
         languages.put("Algol", "a description...");
+        languages.put("Lisp", "another description...");
 
 //        System.out.println(languages.get("Java"));
+
+        languages.remove("Lisp"); // returns true or false
 
         for(String key: languages.keySet()) {
             System.out.println(key + " : " + languages.get(key));
